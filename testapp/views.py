9ca033239b0a26a3bb3229a320cf86f92bd4d0ff
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-02-13 17:43:37 +0500

    Delete testapp directory

--- testapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-def form_view(request):
-    return render(request,"testapp/index.html")
