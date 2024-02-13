72633d3c5242966bdbfa03dcbbaedcfbad5d36d0
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2024-02-13 17:46:03 +0500

    Add New Commit

---
+++ testapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def form_view(request):
+    return render(request,"testapp/index.html")
